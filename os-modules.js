9bcbfdcfbb98d46a192491989857beae62ab6904
Lanre Sam <ezechelanre@gmail.com>
2022-01-29 23:23:28 +0100

    creating customize server

---
+++ os-modules.js
@@ -0,0 +1,25 @@
+//Path Module
+const path = require('path');
+
+console.log(path.sep);
+
+//OS module
+const os = require('os');
+
+//info about current user
+const user = os.userInfo();
+console.log(user);
+
+
+//info about PC uptime
+console.log(`The system uptime is: ${os.uptime()} seconds`);
+
+//info about current OS
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalmem: os.totalmem(),
+    freemem: os.freemem()
+}
+
+console.log(currentOS);
