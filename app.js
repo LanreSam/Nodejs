7d983634ac3a707993ea1cdf1be2b0fd77664dd1
lanreSam <ezechelanre@gmail.com>
2022-03-05 01:23:28 +0100

    setting up server

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-res.write('Welcome to our home page');
-res.end();
+    if (req.url === '/') {
+        res.end('Welcome to our home page');
+    }
+    if (req.url === '/about') {
+        res.end('Here is the about page.');
+    }
+
+    res.end(`<h1>Oops!!!</h1>
+        <p>Page not find</p>
+    `);
+
 });
 
 server.listen(2000);
