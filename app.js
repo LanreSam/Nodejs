d40015fe56c1d6c6b4a6a413747248a8106c49a1
lanreSam <ezechelanre@gmail.com>
2022-03-06 10:59:00 +0100

    asyn

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-require('./http');
+const http = require('http');
+
+
+const server = http.createServer((req, res)=>{
+
+});
+
+
+server.listen(5000, () => {
+    console.log('Server listening to port 5000.....');
+});
