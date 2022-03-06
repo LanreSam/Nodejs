c4bd916dbd0e09f152b076d5adf4ce06ba65a2fd
lanreSam <ezechelanre@gmail.com>
2022-03-06 19:15:12 +0100

    events

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-const http = require('http');
+const eventEmitter = require('events');
 
+const customEmitter = new eventEmitter();
 
-const server = http.createServer((req, res)=>{
-
-});
-
-
-server.listen(5000, () => {
-    console.log('Server listening to port 5000.....');
-});
